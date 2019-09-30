2ad88300b10b8b73c6aebfff8bc8ad903632d281
Isaacddr <isaacovic00@gmail.com>
2019-09-30 18:49:48 -0500

    future changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
-// Flutter code sample for
-
-// This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
-// widget. The [BottomNavigationBar] has three [BottomNavigationBarItem]
-// widgets and the [currentIndex] is set to index 0. The selected item is
-// amber. The `_onItemTapped` function changes the selected item's index
-// and displays a corresponding message in the center of the [Scaffold].
-//
-// ![A scaffold with a bottom navigation bar containing three bottom navigation
-// bar items. The first one is selected.](https://flutter.github.io/assets-for-api-docs/assets/material/bottom_navigation_bar.png)
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'El latir';
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
@@ -37,15 +28,15 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
-      'Restaurantes',
+      'Index 0: Home',
       style: optionStyle,
     ),
     Text(
-      'Tours',
+      'Index 1: Business',
       style: optionStyle,
     ),
     Text(
-      'Calendario',
+      'Index 2: School',
       style: optionStyle,
     ),
   ];
@@ -60,10 +51,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('El latir '),
-        actions:<Widget>[
-          Icon(Icons.search),
-        ],
+        title: const Text('BottomNavigationBar Sample'),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
@@ -71,16 +59,16 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            title: Text('Comida'),
+            icon: Icon(Icons.home),
+            title: Text('Home'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.directions_bus),
-            title: Text('Tours'),
+            icon: Icon(Icons.business),
+            title: Text('Business'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            title: Text('Eventos'),
+            icon: Icon(Icons.school),
+            title: Text('School'),
           ),
         ],
         currentIndex: _selectedIndex,
