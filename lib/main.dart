880751ce531c1b79b3cde17a350434be7fa030a4
Isaacddr <isaacovic00@gmail.com>
2019-10-03 23:11:18 -0500

    Re-set main

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'El app';
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +14,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyStatefulWidget extends StatefulWidget {
   MyStatefulWidget({Key key}) : super(key: key);
 
@@ -24,100 +24,87 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  List<Widget> _widgetOptions = <Widget>[
-    ListView(
-      children: <Widget> [
-        Card(
-          child: InkWell(
-            splashColor: Colors.orange.withAlpha(30),
-            child: Container(
-              width: 300,
-              height: 100,
-              child: Center(
-                child: Text(
-                  "Restaurante",
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
-    ListView(
-      children: <Widget> [
-        Card(
-          child: InkWell(
-            splashColor: Colors.orange.withAlpha(30),
-            child: Container(
-              width: 300,
-              height: 100,
-              child: Center(
-                child: Text(
-                  'Tours',
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
-    ListView(
-      children: <Widget> [
-        Card(
-          child: InkWell(
-            splashColor: Colors.orange.withAlpha(30),
-            child: Container(
-              height: 300,
-              width: 100,
-              child: Center(
-                child: Text(
-                  "Calendario",
-                ),
-              ),
-            ),
-          ),
 
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  List<Widget> _widgetOption = <Widget> [
+    ListView(
+      children: <Widget> [
+        Container(
+          child: Column(
+            children: <Widget> [
+              Text("Uno"),
+            ],
+          ),
         ),
       ],
+    ),
+
+    ListView(
+      children: <Widget> [
+        Container(
+          child: Column(
+            children: <Widget> [
+              Text("Dos"),
+            ],
+          ),
+        ),
+      ],
+    ),
+
+    ListView(
+      children: <Widget> [
+        Container(
+          child: Column(
+            children: <Widget> [
+              Text("Tres"),
+            ],
+          ),
+        ),
+      ],
+
     ),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
+
+  void _onTapped (int index) {
+    setState((){
       _selectedIndex = index;
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
+        title: Text("El App"),
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _widgetOption.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem> [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            title: Text('Home'),
+            title: Text("Uno"),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
+            icon: Icon(Icons.directions_bus),
+            title: Text("Dos"),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('School'),
+            icon: Icon(Icons.calendar_view_day),
+            title: Text("Tres")
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+        onTap: _onTapped,
       ),
     );
   }
 }
+
+
+
