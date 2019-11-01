f89f97df65f2c433c100acaf8a7cbb6d14912d79
IsaacDdR <isaacovic00@gmail.com>
2019-10-31 18:20:58 -0600

    title

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static const String _title = 'Titulo';
+  static const String _title = 'APP';
 
   @override
   Widget build(BuildContext context) {
